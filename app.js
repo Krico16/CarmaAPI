31bd8ddc6a58fb8b9e420b85c98e5ecef1f489ba
Krico16 <bash_stone_u@hotmail.com>
2019-09-12 12:53:37 -0500

    Cambiamos las keys de firebase por seguridad

--- app.js
+++ app.js
@@ -5,12 +5,24 @@ var cookieParser = require('cookie-parser');require('firebase');
 var logger = require('morgan');
 var firebase = require('firebase-admin');
 var token = require('./token/firebase.json');
+const config = {
+  "type": process.env.type,
+  "project_id": process.env.project_id,
+  "private_key_id": process.env.private_key_id,
+  "private_key": process.env.private_key,
+  "client_email": process.env.client_email,
+  "client_id": process.env.client_id,
+  "auth_uri": process.env.auth_uri,
+  "token_uri": process.env.token_uri,
+  "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
+  "client_x509_cert_url": process.env.client_x509_cert_url
+};
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 firebase.initializeApp({
-  credential: firebase.credential.cert(token),
+  credential: firebase.credential.cert(config),
   databaseURL: "https://carma-web-75ffe.firebaseio.com"
 });
 
